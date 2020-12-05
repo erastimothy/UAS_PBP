a66e56824cf14e6b7129a92e0fa98d63fa2b1ef4
Eras <setorantimothy@gmail.com>
2020-12-06 03:51:23 +0700

    "Adding Unit test UI"

---
+++ app/src/androidTest/java/com/erastimothy/laundry_app/RegisterTest.java
@@ -0,0 +1,206 @@
+package com.erastimothy.laundry_app;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RegisterTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnSignUp), withText("New User ? Sign up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                4)));
+        materialButton.perform(scrollTo(), click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.btnSignUp), withText("Sign Up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        materialButton2.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.nama_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0)));
+        textInputEditText.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(R.id.nama_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0)));
+        textInputEditText2.perform(scrollTo(), replaceText("eras timothy"), closeSoftKeyboard());
+
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.btnSignUp), withText("Sign Up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        materialButton3.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(withId(R.id.email_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                1)));
+        textInputEditText3.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText4 = onView(
+                allOf(withId(R.id.email_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0)));
+        textInputEditText4.perform(scrollTo(), replaceText("erastimothy9@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.btnSignUp), withText("Sign Up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        materialButton4.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText5 = onView(
+                allOf(withId(R.id.phoneNumber_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                1)));
+        textInputEditText5.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText6 = onView(
+                allOf(withId(R.id.phoneNumber_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0)));
+        textInputEditText6.perform(scrollTo(), replaceText("08982866621"), closeSoftKeyboard());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.btnSignUp), withText("Sign Up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        materialButton5.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText7 = onView(
+                allOf(withId(R.id.password_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                1)));
+        textInputEditText7.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText8 = onView(
+                allOf(withId(R.id.password_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0)));
+        textInputEditText8.perform(scrollTo(), replaceText("password"), closeSoftKeyboard());
+
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.btnSignUp), withText("Sign Up"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        materialButton6.perform(scrollTo(), click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
